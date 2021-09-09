86ecdfefa5f6c44d3d98018ba0bf318432887d27
Bibek Sapkota <bibeksp7@gmail.com>
2021-09-09 13:10:46 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-// import './App.css';
-// import { Login } from './components/auth/login/login'
-import { NavBar } from '../src/components/shared/navbar';
 import {AppRouting} from './app.routing'
 import { Provider } from 'react-redux';
 import { store } from './store'
-import { Register } from './components/auth/register/register';
 
 export default function App() {
   return (
